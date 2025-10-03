045ab16d52b16331ff09ebf3bdb7876531b06b2f
mohammed <mo.2301@hotmail.com>
2025-10-03 18:57:05 +0300

    Add GitHub

---
+++ src/main/java/com/mod98/alpaca/tradingbot/AlpacaTradingBotApplication.java
@@ -0,0 +1,13 @@
+package com.mod98.alpaca.tradingbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlpacaTradingBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlpacaTradingBotApplication.class, args);
+		System.out.println("Test Add GitHub");
+	}
+}
